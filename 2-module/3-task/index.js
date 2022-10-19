67e5ae51c85044f79b5b32af0c5b9a8420e3f963
TK <ottertut@gmail.com>
2022-10-19 15:19:58 +0300

    Module-2-task-3

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,16 @@
 let calculator = {
-  // ваш код
-};
+    sum() {
+    return this.a + this.b;
+  },
 
+  mul() {
+    return this.a * this.b;
+  },
+
+  read(a,b) {
+    this.a = a
+    this.b = b
+  }
+};
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
