e5d3e94de1dbccd7750b37dd15a788d65511ba2a
vidra <ottertut@gmail.com>
2022-10-21 23:16:14 +0300

    Update index.js

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,8 @@
+let vasya = { name: 'Вася', age: 25 };
+let petya = { name: 'Петя', age: 30 };
+let masha = { name: 'Маша', age: 28 };
+let users = [ vasya, petya, masha ];
 function namify(users) {
-return users.map(item => item.name)
+return users.map(item => item.name);
 }
+let names = namify(users)
