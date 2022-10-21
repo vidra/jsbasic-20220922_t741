6e727010ca25b7b0c44e8f9bccc1945bfc6d7166
vidra <ottertut@gmail.com>
2022-10-21 23:03:13 +0300

    Update index.js

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,15 +1,14 @@
 let calculator = {
-    sum() {
+   sum() {
     return this.a + this.b;
   },
   mul() {
     return this.a * this.b;
   },
-
   read(a,b) {
     this.a = a
     this.b = b
   }
-};
+}
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
